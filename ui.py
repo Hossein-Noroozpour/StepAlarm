38fbb90ae72ed89b6ff440d9f59b955914658bfe
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2014-12-03 12:05:58 +0330

    Starting2

--- ui.py
+++ ui.py
@@ -1,1 +1,18 @@
+from mako.template import _get_module_info_from_callable
+
 __author__ = 'Hossein Noroozpour'
+from gi.repository import Gtk
+
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Step Alarm")
+        grid = Gtk.Grid()
+        self.add(grid)
+
+
+if '__main__' == __name__:
+    window = MainWindow()
+    window.connect("delete-event", Gtk.main_quit)
+    window.show_all()
+    Gtk.main()
